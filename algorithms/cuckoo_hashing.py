4d4802f316407f0fde9fdf38daba54bc9c11cfdb
Alexander Slavutin <alexander.slavutin@xperience.ai>
2020-12-03 21:51:28 +0300

    hash fucntion maker added

--- algorithms/cuckoo_hashing.py
+++ algorithms/cuckoo_hashing.py
@@ -1,19 +1,31 @@
 import hashlib
 from utils import UniversalHashFunction
+from Crypto.Util import number
 
 
 class Node:
-    def __init__(self, data=None):
+    def __init__(self, key=None, data=None):
+        self.key = key
         self.data = data
-        self.next = None
 
 
 class CuckooHashMap:
     def __init__(self, size: int = 20):
+        def get_hash_fucntions(hash_functions_number=2):
+            hash_fucntions = []
+            number_of_bits = 8
+            p = number.getPrime(number_of_bits)
+            assert p > self.size
+            for _ in range(hash_functions_number):
+                hash_fucntions.append(UniversalHashFunction(p, self.size))
+            return hash_fucntions
+
         self.size = size
         self.map_1 = [None] * self.size
         self.map_2 = [None] * self.size
         self.collision_count = 0
+        self.elements_amount = 0
+        self.hash_fucntions = get_hash_fucntions()
 
     def __repr__(self):
         for i in range(self.size):
@@ -30,11 +42,19 @@ class CuckooHashMap:
         else:
             self.map_2[key_hash] = key
 
-    def get(self):
-        pass
+    def get(self, key):
+        first_index = self.hash_fucntions[0](key)
+        second_index = self.hash_fucntions[1](key)
+        print(first_index, second_index)
+
+    def get_the_load_factor(self):
+        return self.elements_amount / self.size
 
     def insert(self):
         pass
 
     def delete(self):
         pass
+
+    def __len__(self):
+        return self.elements_amount
--- main.py
+++ main.py
@@ -5,17 +5,14 @@ from Crypto.Util import number
 
 
 def main():
-    number_of_bits = 8
-    p = number.getPrime(number_of_bits)
-    hash_map_size = 1
-    assert p > hash_map_size
-    h_1 = UniversalHashFunction(p, hash_map_size)
-    h_2 = UniversalHashFunction(p, hash_map_size)
-    h = CuckooHashMap(size=hash_map_size, h_1=h_1, h_2=h_2)
-    digits = [0, 1, 2]
-    for digit in digits:
-        h.add(digit)
-    print(h)
+    hash_map_size = 5
+    h = CuckooHashMap(size=hash_map_size)
+    h.get(5)
+    print(h.get_the_load_factor())
+    # digits = [0, 1, 2]
+    # for digit in digits:
+    #     h.add(digit)
+    # print(h)
 
 
 if __name__ == "__main__":
